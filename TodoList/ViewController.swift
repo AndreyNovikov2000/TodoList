a11f074d9fd0d3f84466534b4b0738952502f04d
Andrey Novikov <andrenovikov2000@mail.ru>
2020-05-10 23:28:25 +0600

    Initial Commit

---
+++ TodoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TodoList
+//
+//  Created by Andrey Novikov on 5/10/20.
+//  Copyright © 2020 Andrey Novikov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
